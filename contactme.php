e79f5b9e53145cb606652f7f6eac945626d131f9
Naran Torguud <ntorguud@gmail.com>
2021-02-01 05:03:40 +0000

    Add properties, move properties

--- contactme.php
+++ contactme.php
@@ -17,212 +17,150 @@
 </head>
 
 <body>
-
-    <div id="wrapper"></div>
-
     <h1><img class="left" src="images/home.png" alt="logo" width="35">Naran Torguud's SCC IT162 Contact Form Page</h1>
+    <br>
 
     <nav>
         <ul>
-            <li><a href="index.html">Welcome</a></li>
-            <li><a href="big.html">Big</a></li>
-            <li><a href="aia.html">AIA</a></li>
-            <li><a href="flowchart.html">Flowchart</a></li>
-            <li><a href="finalproject.html">Final Project</a></li>
-            <li><a href="contactme.html">Contact Naran</a></li>
+            <li><a href="index.php">Welcome</a></li>
+            <li><a href="big.php">Big</a></li>
+            <li><a href="aia.php">AIA</a></li>
+            <li><a href="flowchart.php">Flowchart</a></li>
+            <li><a href="finalproject.php">Final Project</a></li>
+            <li><a href="contactme.php">Contact Naran</a></li>
         </ul>
     </nav>
 
-    <main>
-        <form action="formhandler.php" method="post">
-            <!-- method="post" is secure-->
-            <fieldset>
-                <legend>Join "Michid Dent" Dental Clinic</legend>
-                <label>Name</label>
-                <input type="text" name="name">
+    <div id="wrapper">
+        <main>
+            <form action="formhandler.php" method="post">
+                <!-- method="post" is secure-->
+                <fieldset>
+                    <legend>Join "Michid Dent" Dental Clinic</legend>
+                    <label>First Name</label>
+                    <input type="text" name="name">
 
-                <label>Email</label>
-                <input type="email" name="email">
+                    <label>Last Name</label>
+                    <input type="text" name="name">
 
-                <label>Phone Number</label>
-                <input type="number" name="number">
+                    <label>Enter your birthday:
+                        <input type="date" name="bday"></label>
 
-                <label>Home Address/optional/</label>
-                <input type="text" name="text">
+                    <label>Gender /optional/</label>
+                    <select name="gender">
+                        <option value="gender">Gender</option>
+                        <option value="male">Male</option>
+                        <option value="female">Female</option>
+                    </select>
 
-                <label>City</label>
-                <input type="text" name="text">
+                    <label>Age</label>
+                    <input type="number" name="age">
 
-                <label>State</label>
-                <select name="state">
-                    <option value="state">Choose your state</option>
-                    <option value="alabama">Alabama</option>
-                    <option value="alaska">Alaska</option>
-                    <option value="arizona">Arizona</option>
-                    <option value="arkansas">Arkansas</option>
-                    <option value="california">California</option>
-                    <option value="colorado">Colorado</option>
-                    <option value="connecticut">Connecticut</option>
-                    <option value="delaware">Delaware</option>
-                    <option value="district of columbia">District of Columbia</option>
-                    <option value="florida">Florida</option>
-                    <option value="georgia">Georgia</option>
-                    <option value="hawaii">Hawaii</option>
-                    <option value="idaho">Idaho</option>
-                    <option value="illinois">Illinois</option>
-                    <option value="indiana">Indiana</option>
-                    <option value="iowa">Iowa</option>
-                    <option value="kansas">Kansas</option>
-                    <option value="kentucky">Kentucky</option>
-                    <option value="louisana">Louisana</option>
-                    <option value="maine">Maine</option>
-                    <option value="maryland">Maryland</option>
-                    <option value="massachusetts">Massachusetts</option>
-                    <option value="michigan">Michigan</option>
-                    <option value="minnesota">Minnesota</option>
-                    <option value="mississipi">Mississipi</option>
-                    <option value="missouri">Missouri</option>
-                    <option value="montana">Montana</option>
-                    <option value="nebraska">Nebraska</option>
-                    <option value="nevada">Nevada</option>
-                    <option value="new hampshire">New Hampshire</option>
-                    <option value="new jersey">New Jersey</option>
-                    <option value="new mexico">New Mexico</option>
-                    <option value="new york">New York</option>
-                    <option value="north carolina">North Carolina</option>
-                    <option value="north dakota">North Dakota</option>
-                    <option value="ohio">Ohio</option>
-                    <option value="oklahama">Oklahama</option>
-                    <option value="oregon">Oregon</option>
-                    <option value="pennsylvania">Pennsylvania</option>
-                    <option value="rhode island">Rhode Island</option>
-                    <option value="south carolina">South Carolina</option>
-                    <option value="south dakota">South Dakota</option>
-                    <option value="tennessee">Tennessee</option>
-                    <option value="texas">Texas</option>
-                    <option value="utah">Utah</option>
-                    <option value="velmont">Velmont</option>
-                    <option value="virginia">Virginia</option>
-                    <option value="washington">Washington</option>
-                    <option value="west virginia">West Virginia</option>
-                    <option value="wisconsin">Wisconsin</option>
-                    <option value="wyoming">Wyoming</option>
-                </select>
+                    <label>Email</label>
+                    <input type="email" name="email">
 
-                <label>Gender /optional/</label>
-                <select name="gender">
-                    <option value="gender">Gender</option>
-                    <option value="male">Male</option>
-                    <option value="female">Female</option>
-                </select>
+                    <label>Phone Number</label>
+                    <input type="number" name="number">
 
-                <label>Age</label>
-                <input type="number" name="age">
+                    <label>Home Address/optional/</label>
+                    <input type="text" name="text">
 
-                <input type="submit" value="Join now !">
+                    <label>District</label>
+                    <select name="district">
+                        <option value="district">Choose your district</option>
+                        <option value="baganuur">Baganuur</option>
+                        <option value="bagakhangai">Bagakhangai</option>
+                        <option value="bayangol">Bayangol</option>
+                        <option value="bayanzurkh">Bayanzurkh</option>
+                        <option value="chingeltei">Chingeltei</option>
+                        <option value="khanuul">Khan-Uul</option>
+                        <option value="nalaikh">Nalaikh</option>
+                        <option value="songinokhairkhan">Songinokhairkhan</option>
+                        <option value="sukhbaatar">Sukhbaatar</option>
+                        <option value="none">None of above</option>
+                    </select>
 
-                <label>Where did you hear about us?</label>
-                <ul>
-                    <li><input type="checkbox" name="where[]" value="friend">A friend or colleague</li>
-                    <li><input type="checkbox" name="where[]" value="google">A Recommendation of Family Doctor</li>
-                    <li><input type="checkbox" name="where[]" value="blogspot">An Advertisement</li>
-                    <li><input type="checkbox" name="where[]" value="article">Internet or Medical Journals</li>
-                </ul> <!-- [] is for checking several things -->
+                    <label>City</label>
+                    <input type="text" name="text">
 
-                <legend>Make an Appointment</legend>
-                <label>Name</label>
-                <input type="text" name="name">
+                    <label>Where did you hear about us?</label>
+                    <ul>
+                        <li><input type="checkbox" name="where[]" value="friend">A friend or colleague</li>
+                        <li><input type="checkbox" name="where[]" value="google">A Recommendation of Family Doctor</li>
+                        <li><input type="checkbox" name="where[]" value="blogspot">An Advertisement</li>
+                        <li><input type="checkbox" name="where[]" value="article">Internet or Medical Journals</li>
+                    </ul> <!-- [] is for checking several things -->
 
-                <label>Email</label>
-                <input type="email" name="email">
+                    <input type="submit" value="Join now !">
+                </fieldset>
+                <br>
 
-                <label>Phone Number</label>
-                <input type="number" name="number">
+                <fieldset>
+                    <legend>Make an Appointment</legend>
+                    <label>What service are you looking for?</label>
+                    <ul>
+                        <li><input type="checkbox" name="what[]" value="exams">Complete Exams</li>
+                        <li><input type="checkbox" name="what[]" value="xray">X-Ray</li>
+                        <li><input type="checkbox" name="what[]" value="cleanings">Dental Cleanings</li>
+                        <li><input type="checkbox" name="what[]" value="fillings">Fillings</li>
+                        <li><input type="checkbox" name="what[]" value="rootcanals">Root Canals</li>
+                        <li><input type="checkbox" name="what[]" value="extractions">Extractions</li>
+                        <li><input type="checkbox" name="what[]" value="implants">Implants</li>
+                        <li><input type="checkbox" name="what[]" value="crowns">Crowns</li>
+                        <li><input type="checkbox" name="what[]" value="bridge">Bridge</li>
+                        <li><input type="checkbox" name="what[]" value="dentures">Full/Partial Dentures</li>
+                        <li><input type="checkbox" name="what[]" value="orthodontics">Orthodontics</li>
+                        <li><input type="checkbox" name="what[]" value="oralappliances">Oral Appliances for control of sleep apnea</li>
+                        <li><input type="checkbox" name="what[]" value="whitening">Whitening</li>
+                        <li><input type="checkbox" name="what[]" value="porcelain">Porcelain</li>
+                        <li><input type="checkbox" name="what[]" value="compositeveneers">Composite Veneers</li>
+                        <li><input type="checkbox" name="what[]" value="advice">Preventive Advice</li>
+                    </ul>
+                    <br>
 
-                <label>Home Address/optional/</label>
-                <input type="text" name="text">
+                    <label>What are your Dental Issues?</label>
+                    <textarea name="comments"></textarea>
+                    <br>
 
-                <label>City</label>
-                <input type="text" name="text">
+                    <label>Do you have a Health Insurance?</label>
+                    <select name="insurance">
+                        <option value="insurance"></option>
+                        <option value="yes">Yes</option>
+                        <option value="no">No</option>
+                    </select>
 
-                <label>State</label>
-                <select name="state">
-                    <option value="state">Choose your state</option>
-                    <option value="alabama">Alabama</option>
-                    <option value="alaska">Alaska</option>
-                    <option value="arizona">Arizona</option>
-                    <option value="arkansas">Arkansas</option>
-                    <option value="california">California</option>
-                    <option value="colorado">Colorado</option>
-                    <option value="connecticut">Connecticut</option>
-                    <option value="delaware">Delaware</option>
-                    <option value="district of columbia">District of Columbia</option>
-                    <option value="florida">Florida</option>
-                    <option value="georgia">Georgia</option>
-                    <option value="hawaii">Hawaii</option>
-                    <option value="idaho">Idaho</option>
-                    <option value="illinois">Illinois</option>
-                    <option value="indiana">Indiana</option>
-                    <option value="iowa">Iowa</option>
-                    <option value="kansas">Kansas</option>
-                    <option value="kentucky">Kentucky</option>
-                    <option value="louisana">Louisana</option>
-                    <option value="maine">Maine</option>
-                    <option value="maryland">Maryland</option>
-                    <option value="massachusetts">Massachusetts</option>
-                    <option value="michigan">Michigan</option>
-                    <option value="minnesota">Minnesota</option>
-                    <option value="mississipi">Mississipi</option>
-                    <option value="missouri">Missouri</option>
-                    <option value="montana">Montana</option>
-                    <option value="nebraska">Nebraska</option>
-                    <option value="nevada">Nevada</option>
-                    <option value="new hampshire">New Hampshire</option>
-                    <option value="new jersey">New Jersey</option>
-                    <option value="new mexico">New Mexico</option>
-                    <option value="new york">New York</option>
-                    <option value="north carolina">North Carolina</option>
-                    <option value="north dakota">North Dakota</option>
-                    <option value="ohio">Ohio</option>
-                    <option value="oklahama">Oklahama</option>
-                    <option value="oregon">Oregon</option>
-                    <option value="pennsylvania">Pennsylvania</option>
-                    <option value="rhode island">Rhode Island</option>
-                    <option value="south carolina">South Carolina</option>
-                    <option value="south dakota">South Dakota</option>
-                    <option value="tennessee">Tennessee</option>
-                    <option value="texas">Texas</option>
-                    <option value="utah">Utah</option>
-                    <option value="velmont">Velmont</option>
-                    <option value="virginia">Virginia</option>
-                    <option value="washington">Washington</option>
-                    <option value="west virginia">West Virginia</option>
-                    <option value="wisconsin">Wisconsin</option>
-                    <option value="wyoming">Wyoming</option>
-                </select>
+                    <label>Doctor's Name</label>
+                    <input type="text" name="drname">
 
-                <label>Gender /optional/</label>
-                <select name="gender">
-                    <option value="gender">Gender</option>
-                    <option value="male">Male</option>
-                    <option value="female">Female</option>
-                </select>
+                    <label>Date:
+                        <input type="date" name="appday"></label>
 
-                <input type="submit" value="Submit">
+                    <label>Time:
+                        <input type="time" name="apptime"></label>
 
-                <label>How would you like to receive the latest information?</label>
-                <ul>
-                    <li><input type="radio" name="info" value="email">Email</li>
-                    <li><input type="radio" name="info" value="text">Text Message</li>
-                    <li><input type="radio" name="info" value="mailbox">Mailbox</li>
-                    <li><input type="radio" name="info" value="no info">No Info</li>
-                </ul>
+                    <label>How would you like to receive an important information and notifications?</label>
+                    <ul>
+                        <li><input type="radio" name="info" value="email">Email</li>
+                        <li><input type="radio" name="info" value="text">Text Message</li>
+                        <li><input type="radio" name="info" value="mailbox">Mailbox</li>
+                        <li><input type="radio" name="info" value="no info">No Notification</li>
+                    </ul>
+                    <br>
 
-                <label>Comments</label>
-                <textarea name="comments"></textarea>
-            </fieldset>
-        </form>
-    </main>
+                    <p>* Please have your valid Passport or photo ID, and Medical Story.</p>
+                    <p>* Be prepared to fill out your health related information thoroughly.</p>
+                    <p>* Be ready 15 minutes before your appointment time.</p>
+                    <br>
+
+                    <input type="submit" value="Submit">
+
+                    <label>Comments</label>
+                    <textarea name="comments"></textarea>
+                </fieldset>
+            </form>
+        </main>
+
+    </div>
 
     <footer>
         <ul>
--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <title>My ContactForm HTML page</title>
@@ -16,48 +17,49 @@
 </head>
 
 <body>
+    <h1><img class="left" src="images/home.png" alt="logo" width="35">Naran Torguud's SCC IT162 Portal</h1>
+    <br>
 
-<div id="wrapper">
+    <nav>
+        <ul>
+            <li><a href="index.php">Welcome</a></li>
+            <li><a href="big.php">Big</a></li>
+            <li><a href="aia.php">AIA</a></li>
+            <li><a href="flowchart.php">Flowchart</a></li>
+            <li><a href="finalproject.php">Final Project</a></li>
+            <li><a href="contactme.php">Contact Naran</a></li>
+        </ul>
+    </nav>
 
-<h1><img class="left" src="images/home.png" alt="logo" width="35">Naran Torguud's SCC IT162 Portal</h1>
+    <div id="wrapper">
+        <h2>Welcome</h2>
+        <div class="aside">
+            <div class="blueberry">
+                <ul class="slides">
+                    <li><img src="images/naran.png" alt="image" width="250"></li>
+                    <li><img src="images/naran1.png" alt="images" width="250"></li>
+                    <li><img src="images/naran2.png" alt="images" width="250"></li>
+                </ul>
+            </div>
+        </div>
 
-<nav>
-  <ul>
-    <li><a href="index.php">Welcome</a></li>
-    <li><a href="big.php">Big</a></li>
-    <li><a href="aia.php">AIA</a></li>
-    <li><a href="flowchart.php">Flowchart</a></li>
-    <li><a href="finalproject.php">Final Project</a></li>
-    <li><a href="contactme.php">Contact Naran</a></li>
-  </ul>
-</nav>
+        <p>I am Naran Torguud. I was born and grew up in Mongolia. Since I was in a middle school, I have wondered how people discovered those scientific inventions, such as coding. However, I completed a bachelor's degree in Textile Design in my country, I have never forgotten my dream which is learning code. After I graduated, I started to work at Mongolia's leading Cashmere Fashion company called "Gobi Cashmere" for 5 years.
+        </p>
 
-<h2>Welcome</h2>
-<div class="blueberry">
-  <ul class="slides">
-    <li><img src="images/naran.png" alt="image" width="250"></li>
-    <li><img src="images/naran1.png" alt="images" width="250"></li>
-    <li><img src="images/naran2.png" alt="images" width="250"></li>
-  </ul>
-</div>
+        <p>I decided to change my career in order to fulfill my aspiration, and also wanted know other cultures and life style. Thus, I moved to Seattle, Washington in January 2020. This is my fourth quarter at Seattle Central College. I am planning to complete A.A.S degree of Web Development at Seattle Central College. I love this beautiful Emerald city. In my free time, I like to cook and play volleyball.
+        </p>
+    </div>
 
-  <p>I am Naran Torguud. I was born and grew up in Mongolia. Since I was in a middle school, I have wondered how people discovered those scientific inventions, such as coding. However, I completed a bachelor's degree in Textile Design in my country, I have never forgotten my dream which is learning code. After I graduated, I started to work at Mongolia's leading Cashmere Fashion company called "Gobi Cashmere" for 5 years.
-  </p>
+    <footer>
+        <ul>
+            <li>&copy; <?php echo date('Y'); ?>
+            <li>by <a href="http://central.mystudentswa.com/naranto/web110/">Naran Torguud</a></li>
+            <li>All Rights Reserved</li>
+            <li><a href="https://validator.w3.org/check?uri=referer"><img src="images/html5.png" alt="html5"></a></li>
+            <li><a href="http://jigsaw.w3.org/css-validator/#validate_by_input">Valid CSS</a></li>
+        </ul>
 
-  <p>I decided to change my career in order to fulfill my aspiration, and also wanted know other cultures and life style. Thus, I moved to Seattle, Washington in January 2020. This is my fourth quarter at Seattle Central College. I am planning to complete A.A.S degree of Web Development at Seattle Central College. I love this beautiful Emerald city. In my free time, I like to cook and play volleyball.
-  </p>
-
-
-  <footer>
-    <ul>
-      <li>&copy; <?php echo date('Y');?>
-      <li>by <a href="http://central.mystudentswa.com/naranto/web110/">Naran Torguud</a></li>
-      <li>All Rights Reserved</li>
-      <li><a href="https://validator.w3.org/check?uri=referer"><img src="images/html5.png" alt="html5"></a></li>
-      <li><a href="http://jigsaw.w3.org/css-validator/#validate_by_input">Valid CSS</a></li>
-    </ul>
-
-</footer>   
+    </footer>
 </body>
 
 </html>
