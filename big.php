4987e530c5cd8748d4e19bb2c81803497aa280e8
Naran Torguud <ntorguud@gmail.com>
2021-03-03 06:50:23 +0000

    Embed Youtube video link

--- big.php
+++ big.php
@@ -39,6 +39,10 @@
             <iframe src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=America%2FLos_Angeles&amp;src=bnRvcmd1dWRAZ21haWwuY29t&amp;src=YWRkcmVzc2Jvb2sjY29udGFjdHNAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&amp;src=ZW4udXNhI2hvbGlkYXlAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&amp;color=%23039BE5&amp;color=%2333B679&amp;color=%230B8043" style="border:solid 1px #777" width="800" height="600" frameborder="0" scrolling="no">
             </iframe>   
         </main>
+        <aside>
+            <iframe src="https://youtu.be/-ao_Kc_8rpE">
+            </iframe>
+        </aside>
     </div>
 
     <header>
