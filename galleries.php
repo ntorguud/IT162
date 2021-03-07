223c9290d4e6cd684d6dc032b23bf0d0b05191b0
Naran Torguud <ntorguud@gmail.com>
2021-03-07 23:46:31 +0000

    Add text

---
+++ galleries.php
@@ -0,0 +1,67 @@
+  <!DOCTYPE html>
+  <html>
+
+  <head>
+      <meta charset="utf-8" />
+      <title>Naran Torguud's SCC IT162 "Research2" Page</title>
+      <meta name="viewport" content="initial-scale=1">
+      <meta name="robots" content="noindex,nofollow" />
+      <link rel="stylesheet" type="text/css" href="css/blueberry.css">
+      <link href="css/forms.css" type="text/css" rel="stylesheet">
+
+      <!--[if ltIE9]>
+       <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+     <![endif]-->
+  </head>
+
+  <body>
+      <h1><img class="left" src="images/home.png" alt="logo" width="35">Naran Torguud's SCC IT162 Research2 Page</h1>
+      <br>
+
+      <nav>
+          <ul>
+              <li><a href="index.php">Welcome</a></li>
+              <li><a href="big.php">Big</a>
+                  <ul class="drop-down">
+                      <li><a href="responsive_vs_mobile.php">Research 1</a></li>
+                      <li><a href="galleries.php">Research 2</a></li>
+                      <li><a href="flexbox.php">Research 3</a></li>
+                  </ul>
+              </li>
+              <li><a href="aia.php">AIA</a></li>
+              <li><a href="flowchart.php">Flowchart</a></li>
+              <li><a href="finalproject.php">Final Project</a></li>
+              <li><a href="contactme.php">Contact Naran</a></li>
+          </ul>
+      </nav>
+
+      <div id="wrapper">
+          <h1>Galleries</h1>
+          <p>An image gallery is essential for the websites because website visitors are scared of long texts. Instead, they rather websites which have images, graphics, and even several sliding images at once (IONOS Digitalguide). There are varieties of methods to create photo galleries.
+              First method is embedding a Lightbox image gallery with source code in JavaScript. Through this method, which was developed by Lokesh Dhaka’s in 2005 (IONOS Digitalguide), the photo gallery will be implemented via HTML.
+              Another method is the Image gallery extension for content management systems. This is the pre-built framework for users, even the users who have no IT knowledge. For example, for the open source content management system named WordPress, NextGen, Envira, and Polaroid are the beautiful, and accessible galleries.
+          </p>
+
+          <p>Mobile applications, on the other hand, much safer than responsive websites, and can work offline. You always need the internet in order to access responsive webpages. Mobile apps have better performance, and it is consistent with its operating system.</p>
+          <br>
+          <h2>Works Cited:</h2>
+          <p>Babich, Nick. “Deciding Between Responsive Web or Native Apps.” Xd Ideas, 7 Apr 2020, https://xd.Adobe.com/ideas/principles/web-design/responsive-web-or-native-app/, Accessed 6 Feb 2021.
+
+              Karol, Viktor. “Responsive Website vs Mobile App: Comparison.” ThinkMobiles, Updated 18 Dec 2020, https://thinkmobiles.com/blog/responsive-website-vs-mobile-app/, Accessed 6 Feb 2021.
+          </p>
+      </div>
+
+      <header>
+          <h3>Contact Us!</h3>
+      </header>
+      </main>
+      <footer>
+          <small>&copy; 2013-<?= date('Y') ?>, All Rights Reserved ~
+              <a href="http://validator.w3.org/check/referer" target="_blank">Valid HTML</a> ~
+              <a href="http://jigsaw.w3.org/css-validator/check?uri=referer" target="_blank">Valid CSS</a>
+          </small>
+      </footer>
+      <!-- END Footer -->
+  </body>
+
+  </html>
