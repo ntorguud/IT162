d73e2d35485dfcc233b721f2720c7edf569db293
Naran Torguud <ntorguud@gmail.com>
2021-03-08 20:33:13 +0000

    Add personal link

--- index.php
+++ index.php
@@ -59,7 +59,7 @@
     <footer>
         <ul>
             <li>&copy; <?php echo date('Y'); ?>
-            <li>by <a href="http://central.mystudentswa.com/naranto/web110/">Naran Torguud</a></li>
+            <li>by <a href="http://hackergirl.xyz/index.php">Naran Torguud</a></li>
             <li>All Rights Reserved</li>
             <li><a href="https://validator.w3.org/check?uri=referer"><img src="images/html5.png" alt="html5"></a></li>
             <li><a href="http://jigsaw.w3.org/css-validator/#validate_by_input">Valid CSS</a></li>
