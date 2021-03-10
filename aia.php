41e8d6826ca01efa3c57c3a17bca5aa02aa856f1
Naran Torguud <ntorguud@gmail.com>
2021-03-10 21:09:12 +0000

    Add table

---
+++ aia.php
@@ -0,0 +1,91 @@
+      <!DOCTYPE html>
+      <html>
+
+      <head>
+          <meta charset="utf-8" />
+          <title>Naran Torguud's SCC IT162 AIA Page</title>
+          <meta name="viewport" content="initial-scale=1">
+          <meta name="robots" content="noindex,nofollow" />
+          <link rel="stylesheet" type="text/css" href="css/blueberry.css">
+          <link href="css/styles.css" type="text/css" rel="stylesheet">
+
+          <!--[if ltIE9]>
+       <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+     <![endif]-->
+      </head>
+
+      <body>
+          <h1><img class="left" src="images/home.png" alt="logo" width="35">AIA by Naran</h1>
+          <br>
+
+          <nav id="primary">
+              <ul>
+                  <li><a href="index.php">Welcome</a></li>
+                  <li><a href="big.php">Big</a>
+                      <ul class="drop-down">
+                          <li><a href="responsive_vs_mobile.php">Research 1</a></li>
+                          <li><a href="galleries.php">Research 2</a></li>
+                          <li><a href="flexbox.php">Research 3</a></li>
+                      </ul>
+                  </li>
+                  <li><a href="aia.php">AIA</a></li>
+                  <li><a href="flowchart.php">Flowchart</a></li>
+                  <li><a href="finalproject.php">Final Project</a></li>
+                  <li><a href="contactme.php">Contact Naran</a></li>
+              </ul>
+          </nav>
+
+          <div id="wrapper">
+              <div class="main">
+                  <h1>Audience, Issues, and Approach (AIA)</h1>
+                  <table>
+                      <tr>
+                          <td>Client's name and the final decision maker</td>
+                          <td>Client's business (website)</td>
+                          <td>Redesign or brand new website?</td>
+                          <td>Type of website</td>
+                          <td>Client's goal(s)?</td>
+                      </tr>
+                      <tr>
+                          <th>Enkhzul Jargalsaikhan</th>
+                          <th>"Michid Dent" Dental Clinic</th>
+                          <th>New website</th>
+                          <th>Marketing, community, informational, and expand online presence.</th>
+                          <th>Introducing her dental clinic services, making appointments online, and contacting with customers through chatbot.</th>
+                      </tr>
+                      <tr>
+                          <td>Captain CSS</td>
+                          <td>Superlayout</td>
+                          <td>Lord Deprecated</td>
+                      </tr>
+                      <tr>
+                          <td>Browser Woman</td>
+                          <td>Megacompatibility</td>
+                          <td>Ugly Code Monster</td>
+                      </tr>
+                  </table>
+              </div>
+
+              <div class="container">
+
+              </div>
+
+              <div class="aside">
+
+              </div>
+          </div>
+
+          <header>
+
+          </header>
+
+          <footer>
+              <small>&copy; 2013-<?= date('Y') ?>, All Rights Reserved ~
+                  <a href="http://validator.w3.org/check/referer" target="_blank">Valid HTML</a> ~
+                  <a href="http://jigsaw.w3.org/css-validator/check?uri=referer" target="_blank">Valid CSS</a>
+                  <a href="hackergirl.xyz/index.php" target="_blank">by Naran Torguud</a>
+              </small>
+          </footer>
+      </body>
+
+      </html>
