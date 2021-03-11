83f9b815ae1f279cfd62ac8043f7d22638f812c0
Naran Torguud <ntorguud@gmail.com>
2021-03-11 21:26:23 +0000

    Fix links

--- big.php
+++ big.php
@@ -119,7 +119,7 @@
               <small>&copy; 2013-<?= date('Y') ?>, All Rights Reserved ~
                   <a href="http://validator.w3.org/check/referer" target="_blank">Valid HTML</a> ~
                   <a href="http://jigsaw.w3.org/css-validator/check?uri=referer" target="_blank">Valid CSS</a>
-                  <a href="hackergirl.xyz/index.php" target="_blank">by Naran Torguud</a>
+                  <a href="index.php" target="_blank">by Naran Torguud</a>
               </small>
           </footer>
           <!-- END Footer -->
--- flexbox.php
+++ flexbox.php
@@ -68,7 +68,7 @@
           <small>&copy; 2013-<?= date('Y') ?>, All Rights Reserved ~
               <a href="http://validator.w3.org/check/referer" target="_blank">Valid HTML</a> ~
               <a href="http://jigsaw.w3.org/css-validator/check?uri=referer" target="_blank">Valid CSS</a>
-              <a href="hackergirl.xyz/big.php" target="_blank">by Naran Torguud</a>
+              <a href="big.php" target="_blank">by Naran Torguud</a>
           </small>
       </footer>
       <!-- END Footer -->
