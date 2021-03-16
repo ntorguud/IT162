5bc872bbdf8ad5a7bcf83b43ef3078d0c777624e
Naran Torguud <ntorguud@gmail.com>
2021-03-16 23:14:59 +0000

    Add contents

--- includes/portal-config.php
+++ includes/portal-config.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
 portal-config.php
 
@@ -10,13 +9,38 @@ Used to store all of our WEB120 configuration information
 //prevents data from being sent early
 ob_start();
 
+//this helps us avoid PHP date errors:
+date_default_timezone_set('America/Los_Angeles');
+
+
 //echo basename($_SERVER['PHP_SELF']);
 
 define('THIS_PAGE',basename($_SERVER['PHP_SELF']));
 
-echo THIS_PAGE;
+//echo THIS_PAGE;
 
-die;
+//die;
+
+switch(THIS_PAGE){
+
+    case 'index.php':
+        $title = "Sara's WEB120 Title Page";
+        $logo = 'fa-home';
+        $PageID = 'Welcome';
+    break;
+    
+    case 'contactme.php':
+        $title = "Naran Torguud's IT162 Contact Page";
+        $logo = 'fa-pencil-square-o';
+        $PageID = 'Contact Naran';
+    break;
+
+    default:
+        $title = THIS_PAGE;
+        $logo = 'fa-home';
+        $PageID = 'Welcome';
+   }
+
 
 ?>
 
