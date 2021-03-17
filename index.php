381938754f382de35842014adec1f2daa4bc1f55
ntorguud06 <jenny.oyuka1@gmail.com>
2021-03-17 18:45:59 +0000

    Recover nav bar

--- index.php
+++ index.php
@@ -17,8 +17,21 @@
 </head>
 
 <body>
-    <!-- <h1><img class="left" src="images/home.png" alt="logo" width="35">Naran Torguud's SCC IT162 Portal</h1>
-    <br> -->
+    <h1><img class="left" src="images/home.png" alt="logo" width="35">Naran Torguud's SCC IT162 Portal</h1>
+    <br>
+
+    <nav>
+    <ul class="topnav" id="myTopnav">
+      <li><a href="index.php" class="selected">Welcome</a></li>
+      <li><a href="big/index.php">Big</a></li>
+      <li><a href="aia.php">AIA</a></li>
+      <li><a href="flowchart.php">Flowchart</a></li>
+      <li><a href="fp/index.php">Final Project</a></li>
+      <li><a href="contactme.php">Contact Sara</a></li>
+      <li class="icon"> <a href="javascript:void(0);" onclick="myFunction()">&#9776;</a> </li>
+    </ul>
+  </nav>
+
 
     <div id="wrapper">
         <h2>Welcome</h2>
