1e6a14d6580fe651acdeac59d8c17e33de01183a
ntorguud06 <jenny.oyuka1@gmail.com>
2021-03-17 18:41:52 +0000

    Add array to the function

--- includes/portal-config.php
+++ includes/portal-config.php
@@ -1,4 +1,15 @@
 <?php
+
+//place URL & labels in the array here for navigation:
+$nav1['index.php'] = "Welcome";
+$nav1['big/index.php'] = "Big";
+$nav1['aia.php'] = "AIA";
+$nav1['flowchart.php'] = "Flowchart";
+$nav1['fp/index.php'] = "Final Project";
+$nav1['contactme.php'] = "Contact Naran";
+
+$logo_color = ''; //make logo_color an empty string by default
+
 /*
 portal-config.php
 
@@ -20,8 +31,6 @@ define('THIS_PAGE',basename($_SERVER['PHP_SELF']));
 //echo THIS_PAGE;
 
 //die;
-
-$logo_color = ''; //make logo_color an empty string by default
 
 switch(THIS_PAGE){
 
@@ -50,5 +59,27 @@ switch(THIS_PAGE){
         $logo = '';//no icon by default  
         $PageID = 'Welcome';
    }
+   
+   /*
+makeLinks function will create our dynamic nav when called.
+Call like this:
+echo makeLinks($nav1); #in which $nav1 is an associative array of links
+*/
+function makeLinks($linkArray)
+{
+    $myReturn = '';
+
+    foreach($linkArray as $url => $text)
+    {
+        if($url == THIS_PAGE)
+        {//selected page - add class reference
+	    	$myReturn .= '<li><a class="selected" href="' . $url . '">' . $text . '</a></li>' . PHP_EOL;
+    	}else{
+	    	$myReturn .= '<li><a href="' . $url . '">' . $text . '</a></li>'  . PHP_EOL;
+    	}    
+    }
+      
+    return $myReturn;    
+}
 ?>
 
