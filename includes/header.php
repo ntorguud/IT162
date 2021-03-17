285751f5a26bd9fc2258046d5c0cdb166b0cbe23
ntorguud06 <jenny.oyuka1@gmail.com>
2021-03-17 18:18:44 +0000

    Add font awesome icon link

--- includes/header.php
+++ includes/header.php
@@ -3,3 +3,7 @@
 <html lang="en">
 <head>
 <title><?=$title?></title>
+<script src="https://use.fontawesome.com/ea1242b543.js"></script>
+</head>
+
+<h1><i class="logo fa <?=$logo?>"<?=$logo_color?>></i>Naran Torguud's SCC IT162 Portal</h1>
