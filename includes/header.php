d3704ebb4428009348ca2a6149dbd37048f5f816
ntorguud06 <jenny.oyuka1@gmail.com>
2021-03-17 18:43:46 +0000

    Add nav properties

--- includes/header.php
+++ includes/header.php
@@ -7,3 +7,17 @@
 </head>
 
 <h1><i class="logo fa <?=$logo?>"<?=$logo_color?>></i>Naran Torguud's SCC IT162 Portal</h1>
+
+<nav>
+        <ul class="topnav" id="myTopnav">
+            <?= makeLinks($nav1) ?>
+            <!-- START COMMENT OUT NAV
+      <li><a href="index.php" class="selected">Welcome</a></li>
+      <li><a href="big/index.php">Big</a></li>
+      <li><a href="aia.php">AIA</a></li>
+      <li><a href="flowchart.php">Flowchart</a></li>
+      <li><a href="fp/index.php">Final Project</a></li>
+      <li><a href="contactme.php">Contact Sara</a></li>
+	END COMMENT OUT NAV -->
+        </ul>
+    </nav>
