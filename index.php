729ec2d35d44ddc25e57d39ccca9075b6c7363a7
ntorguud06 <jenny.oyuka1@gmail.com>
2021-03-17 18:33:58 +0000

    Format document

--- index.php
+++ index.php
@@ -21,16 +21,16 @@
     <br>
 
     <nav>
-    <ul class="topnav" id="myTopnav">
-      <li><a href="index.php" class="selected">Welcome</a></li>
-      <li><a href="big/index.php">Big</a></li>
-      <li><a href="aia.php">AIA</a></li>
-      <li><a href="flowchart.php">Flowchart</a></li>
-      <li><a href="fp/index.php">Final Project</a></li>
-      <li><a href="contactme.php">Contact Naran</a></li>
-      <li class="icon"> <a href="javascript:void(0);" onclick="myFunction()">&#9776;</a> </li>
-    </ul>
-  </nav>
+        <ul class="topnav" id="myTopnav">
+            <li><a href="index.php" class="selected">Welcome</a></li>
+            <li><a href="big/index.php">Big</a></li>
+            <li><a href="aia.php">AIA</a></li>
+            <li><a href="flowchart.php">Flowchart</a></li>
+            <li><a href="fp/index.php">Final Project</a></li>
+            <li><a href="contactme.php">Contact Naran</a></li>
+            <li class="icon"> <a href="javascript:void(0);" onclick="myFunction()">&#9776;</a> </li>
+        </ul>
+    </nav>
 
     <div id="wrapper">
         <h2>Welcome</h2>
