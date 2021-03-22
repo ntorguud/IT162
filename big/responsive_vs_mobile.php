ac623ed06ad1be88d1ff90c4462f77bcc6b0ee27
ntorguud03 <jenny.oyuka@gmail.com>
2021-03-22 18:48:27 +0000

    Remove duplicated elements which is already in header.php

--- big/responsive_vs_mobile.php
+++ big/responsive_vs_mobile.php
@@ -1,63 +1,20 @@
-<?php include 'it162/includes/header.php' ?>
+<?php include 'includes/header.php' ?>
 
-<!doctype html>
-<html lang='en'>
-<head>
-    <title>Naran Torguud's IT162 Big Page</title>
-    <meta name="viewport" content="width=device-width initial-scale=1"/>
-    <meta name="robots" content="noindex,nofollow"/>
-    <meta charset='utf-8'>
-    <link rel="stylesheet" href="css/nav.css"/>
-    <link rel="stylesheet" href="css/big.css"/>
-    <link rel="stylesheet" href="css/forms.css"/>
+<div id="wrapper">
+    <section>
+        <h1>Responsive Webpage versus Mobile Application</h1>
+        <p>In modern days, it's a customer's choice whether to use a responsive website or mobile application due to their advantages over other one. While mobile (native) apps are most likely targeted to the regular customers due to their safety, responsive webpages are targeted to new, and occasional customers because of their fast and easy accessibility.
+        </p>
+        <p>What are advantages of tesponsive websites? As I mentioned before, those are easy to access. Besides you are supposed to download and install mobile apps from AppStore or PlayStore, you don't have to download anything when you are using responsive websites. They work equally on any browsers as well. Responsive websites are cheaper to build than making mobile apps. These kinds of websites are more shareable; you can share only the URL. In addition, responsive websites can be found on search engines, such as Google etc. compared to native apps. However, all of these facts do not mean that responsive webpage is the best choice.
+        </p>
+        <p>Mobile applications, on the other hand, much safer than responsive websites, and can work offline. You always need the internet in order to access responsive webpages. Mobile apps have better performance, and it is consistent with its operating system.</p>
+    </section>
 
-    <script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
-    <script src="https://s3.amazonaws.com/menumaker/menumaker.min.js" type="text/javascript"></script>
-    <script src="js/big.script.js"></script>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css"/>
-</head>
-
-  <body>
-
-      <div id="cssmenu">
-          <ul>
-              <li><a href="../index.php"><span><i class="fa fa-fw fa-bank"></i> IT162</span></a></li>
-              <li><a href="index.php"><span><i class="fa fa-fw fa-home"></i> BIG</span></a></li>
-              <li><a href="flexbox.php"><span><i class="fa fa-fw fa-cube"></i> Flexbox</span></a></li>
-              <li><a href="galleries.php"><span><i class="fa fa-fw fa-camera-retro"></i> Galleries</span></a></li>
-              <li><a href="#"><span><i class="fa fa-fw fa-chevron-down"></i> Google</span></a>
-                  <ul>
-                      <li><a href="calendar.php"><span><i class="fa fa-fw fa-calendar"></i> Calendar</span></a></li>
-                      <li><a href="map.php"><span><i class="fa fa-fw fa-map-o"></i> Map</span></a></li>
-                      <li><a href="youtube.php"><span><i class="fa fa-fw fa-youtube-square"></i> YouTube</span></a></li>
-                  </ul>
-              </li>
-              <li><a href="responsive_vs_mobile.php"><span><i class="fa fa-fw fa-html5"></i> Responsive vs Mobile App</span></a></li>
-              <li><a href="webcam.php"><span><i class="fa fa-fw fa-eye"></i> Web Cam</span></a></li>
-          </ul>
-      </div>
-
-      <div id="wrapper">
-          <section>
-              <h1>Responsive Webpage versus Mobile Application</h1>
-              <p>In modern days, it's a customer's choice whether to use a responsive website or mobile application due to their advantages over other one. While mobile (native) apps are most likely targeted to the regular customers due to their safety, responsive webpages are targeted to new, and occasional customers because of their fast and easy accessibility.
-              </p>
-              <p>What are advantages of tesponsive websites? As I mentioned before, those are easy to access. Besides you are supposed to download and install mobile apps from AppStore or PlayStore, you don't have to download anything when you are using responsive websites. They work equally on any browsers as well. Responsive websites are cheaper to build than making mobile apps. These kinds of websites are more shareable; you can share only the URL. In addition, responsive websites can be found on search engines, such as Google etc. compared to native apps. However, all of these facts do not mean that responsive webpage is the best choice.
-              </p>
-              <p>Mobile applications, on the other hand, much safer than responsive websites, and can work offline. You always need the internet in order to access responsive webpages. Mobile apps have better performance, and it is consistent with its operating system.</p>
-          </section>
-
-          <aside>
-              <h2>Works Cited:</h2>
-              <p>Babich, Nick. “Deciding Between Responsive Web or Native Apps.” Xd Ideas, 7 Apr 2020, https://xd.Adobe.com/ideas/principles/web-design/responsive-web-or-native-app/, Accessed 6 Feb 2021.
-              </p>
-              <p>Karol, Viktor. “Responsive Website vs Mobile App: Comparison.” ThinkMobiles, Updated 18 Dec 2020, https://thinkmobiles.com/blog/responsive-website-vs-mobile-app/, Accessed 6 Feb 2021.
-              </p>
-          </aside>
-      </div>
-
-  </body>
-
-  </html>
-
-  <?php include 'it162/includes/footer.php' ?>
+    <aside>
+        <h2>Works Cited:</h2>
+        <p>Babich, Nick. “Deciding Between Responsive Web or Native Apps.” Xd Ideas, 7 Apr 2020, https://xd.Adobe.com/ideas/principles/web-design/responsive-web-or-native-app/, Accessed 6 Feb 2021.
+        </p>
+        <p>Karol, Viktor. “Responsive Website vs Mobile App: Comparison.” ThinkMobiles, Updated 18 Dec 2020, https://thinkmobiles.com/blog/responsive-website-vs-mobile-app/, Accessed 6 Feb 2021.
+        </p>
+    </aside>
+<?php include 'includes/footer.php' ?>
