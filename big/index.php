d2817b3133c4565be7fe5f91b6110e54b81970ab
ntorguud03 <jenny.oyuka@gmail.com>
2021-03-23 18:34:10 +0000

    Change the path of included php files

--- big/index.php
+++ big/index.php
@@ -1,16 +1,19 @@
-<?php include './includes/header.php'?>
+<?php include './big-header.php' ?>
 
 <div id="wrapper">
     <section>
-        <h1>Welcome</h1>
-        <p>Develop and place an updated version of the "multiple.php" contact form on this main page. Adjust its fields ta ask the basic questions you'd always need to ask any client to help determine if you can help them with their website needs.</p>
-        <form><?php include 'includes/multiple.php' ?></form>
+        <h2><?=$PageID?></h2>
+        <p>Develop and place an updated version of the "big-multiple.php" contact form on this main page. Adjust its fields ta ask the basic questions you'd always need to ask any client to help determine if you can help them with their website needs.</p>
+        <form><?php include './big-multiple.php' ?></form>
     </section>
 
     <aside>
-        <h2>Basic Website Design Cycle</h2>
+        <h3>Basic Website Design Cycle</h3>
     </aside>
     <p>Provide at least 3 (linked) resources that help you and/or your web team with steps found in very basic website design and development cycle. For example, client surveys, wireframing tools, etc.</p>
     <p>Here's a website to help get you started, <a href="https://business.tutsplus.com/articles/how-to-extract-the-facts-with-a-web-design-client-questionnaire--fsw-1830" target="top">https://business.tutsplus.com/articles/...</a>
     </p>
-<?php include './includes/footer.php'?>
+<?php include './big-footer.php' ?>
+</div>
+</body>
+</html>
