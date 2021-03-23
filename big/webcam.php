fd7daa0fac6533805932254536df6a28deaf8374
ntorguud03 <jenny.oyuka@gmail.com>
2021-03-23 18:57:20 +0000

    Add contents

---
+++ big/webcam.php
@@ -0,0 +1,17 @@
+<?php include 'includes/big-header.php' ?>
+
+<div id="wrapper">
+    <section>
+        <h2><?= $PageID ?></h2>
+        <p>Iframe 2 different kinds of web cams on to this page. Left column will be a live youtube video </p>
+    </section>
+
+    <aside>
+        <h3>DOT Camera</h3>
+    </aside>
+    <p>Left column will be a live Department of transportation video that comes from a different web page.</p>
+    <?php include 'includes/big-footer.php' ?>
+</div>
+</body>
+
+</html>
