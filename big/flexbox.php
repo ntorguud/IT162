eac984e4456637576b29248233498147d77195bd
ntorguud03 <jenny.oyuka@gmail.com>
2021-03-23 18:33:40 +0000

    Make titles dynamic

--- big/flexbox.php
+++ big/flexbox.php
@@ -2,7 +2,7 @@
 
   <div id="wrapper">
       <section>
-          <h1>Flexbox</h1>
+          <h2><?=$title?></h2>
           <p>According to the W3C, there are three types of Flexbox syntax in modern browsers. Those are:
               <ul>
                   <li>new syntax which is "display: flex;"</li>
@@ -13,7 +13,7 @@
       </section>
 
       <aside>
-          <h2>Works Cited:</h2>
+          <h3>Works Cited:</h3>
           <p>“CSS Flexbox.” CSS Flexbox (Flexible Box), w3schools.com, www.w3schools.com/css/css3_flexbox.asp. Accessed 11 Feb, 2021.
           </p>
           <p>“Flexbox: Browser Support.” CSS Mine: Blog and e-Book on CSS3 and Web UI Development, www.cssmine.com/ebook/flexbox-browsers. Accessed 11 Feb, 2021.
--- big/galleries.php
+++ big/galleries.php
@@ -3,7 +3,7 @@
 
   <div id="wrapper">
       <section>
-          <h1>Galleries</h1>
+          <h2><?=$title?></h2>
           <p>An image gallery is essential for the websites because website visitors are scared of long texts. Instead, they rather websites which have images, graphics, and even several sliding images at once (IONOS Digitalguide). There are varieties of methods to create photo galleries. First method is embedding a Lightbox image gallery with source code in JavaScript. Through this method, which was developed by Lokesh Dhaka’s in 2005 (IONOS Digitalguide), the photo gallery will be implemented via HTML. Another method is the Image gallery extension for content management systems. This is the pre-built framework for users, even the users who have no IT knowledge. For example, for the open source content management system named WordPress, NextGen, Envira, and Polaroid are the beautiful, and accessible galleries.
           </p>
           <p>
@@ -20,7 +20,7 @@
       </section>
 
       <aside>
-          <h2>Works Cited:</h2>
+          <h3>Works Cited:</h3>
           <p>“10 Best Carousel Plugins In JQuery/JavaScript/CSS (2021 Update).” JQuery Plugins, www.jqueryscript.net/blog/best-carousel.html.
           </p>
           <p>“11 Best Free HTML CSS Image Gallery.” Wpshopmart, 2 Sept. 2020, wpshopmart.com/best-html-css-image-gallery/amp/.
