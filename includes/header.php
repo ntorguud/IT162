d46036bd9142bc23c892d87b375233f8db3335bb
ntorguud03 <jenny.oyuka@gmail.com>
2021-03-23 23:53:57 +0000

    Change nav to css menu maker nav

--- includes/header.php
+++ includes/header.php
@@ -7,15 +7,14 @@
     <script src="https://use.fontawesome.com/ea1242b543.js"></script>
 </head>
 
-<h1><i class="logo fa <?= $logo ?>" <?= $logo_color ?>></i>Naran Torguud's SCC IT162 Portal</h1>
+<h1><i class="logo fa <?= $logo ?>" <?= $logo_color ?>></i><?=$PageID?></h1>
 
-<nav>
-    <ul class="topnav" id="myTopnav">
-        <li><a href="index.php" class="selected">Welcome</a></li>
-        <li><a href="big/index.php">Big</a></li>
-        <li><a href="aia.php">AIA</a></li>
-        <li><a href="flowchart.php">Flowchart</a></li>
-        <li><a href="fp/index.php">Final Project</a></li>
-        <li><a href="contactme.php">Contact Naran</a></li>
-    </ul>
-</nav>
+<div id="cssmenu">
+  <ul>
+     <li><a href="index.php"><span><i class="fa fa-fw fa-home"></i> Portal</span></a></li>
+     <li><a href="big/index.php"><span><i class="fa fa-fw fa-bank"></i> BIG</span></a></li>
+     <li><a href="aia.php"><span><i class="fa fa-fw fa-table"></i> AIA</span></a></li>
+     <li><a href="flowchart.php"><span><i class="fa fa-fw fa-pencil-square-o"></i> Flowchart</span></a></li>
+     <li><a href="fp/index.php"><span><i class="fa fa-fw fa-universal-access"></i> Final Project</span></a></li>
+     <li><a href="contactme.php"><span><i class="fa fa-fw fa-phone"></i> Contact Naran</span></a></li>
+  </ul>
