5bff4eaefa553ea8d83a669b9fdd310319b485ef
ntorguud03 <jenny.oyuka@gmail.com>
2021-03-24 05:20:02 +0000

    Add youtube video embedded link

--- big/youtube.php
+++ big/youtube.php
@@ -5,12 +5,16 @@
 
         <div class="section">
             <h2><?= $PageID ?></h2>
-            <p>Iframe a responsive YouTube video.</p>
+            <div>
+                <p>Check out this 15+ minutes video about Accessible Web Design.</p>
+                <iframe width="100%" height="400" src="https://www.youtube.com/embed/-ao_Kc_8rpE" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
+                </iframe>
+            </div>
         </div>
 
         <div class="aside">
             <h3>Video Info</h3>
-            <p>Explain what the video is about.</p>
+            <p>Read Pablo's post about Accessibility: https://uxdesign.cc/designing-for-accessibility-is-not-that-hard-c04cc4779d94</p>
 
         </div>
     </div>
