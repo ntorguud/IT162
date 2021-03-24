c65e03a7dd4184ed0434101c761ca4434a5df6c2
ntorguud03 <jenny.oyuka@gmail.com>
2021-03-24 01:12:59 +0000

    Edit galleries

--- big/galleries.php
+++ big/galleries.php
@@ -2,24 +2,47 @@
 
   <body>
       <div id="wrapper">
-          <section>
-              <h2><?= $PageID ?></h2>
-              <p>An image gallery is essential for the websites because website visitors are scared of long texts. Instead, they rather websites which have images, graphics, and even several sliding images at once (IONOS Digitalguide). There are varieties of methods to create photo galleries. First method is embedding a Lightbox image gallery with source code in JavaScript. Through this method, which was developed by Lokesh Dhaka’s in 2005 (IONOS Digitalguide), the photo gallery will be implemented via HTML. Another method is the Image gallery extension for content management systems. This is the pre-built framework for users, even the users who have no IT knowledge. For example, for the open source content management system named WordPress, NextGen, Envira, and Polaroid are the beautiful, and accessible galleries.
-              </p>
-              <p>
-                  <ul>
-                      <li><img class="gallery" src="images/hexagon.jpeg" alt="image">1. Hexagon Gallery</li>
-                      <li><img class="gallery" src="images/zoomwalljs.jpeg" alt="image">2. Zoomwall.js</li>
-                      <li><img class="gallery" src="images/simple_tab.jpeg" alt="image">3. Simple Tab Gallery</li>
-                      <li><img class="gallery" src="images/purejs_thumbnail.jpeg" alt="image">4. Pure JavaScript Fullscreen Gallery with Thumbnail Navigation</li>
-                      <li><img class="gallery" src="images/minimalist_touch.jpeg" alt="image">5. Minimalist Touch-friendly Gallery in JavaScript</li>
-                      <li><img class="gallery" src="images/flow.jpeg" alt="image">6. Flow Gallery</li>
-                      <li><img class="gallery" src="images/material_resp.jpeg" alt="image">7. Material Responsive Photo Gallery</li>
-                  </ul>
-              </p>
-          </section>
 
-          <aside>
+          <div> <!-- starts left column -->
+              <div>
+                  <h2><?= $PageID ?></h2>
+                  <p>An image gallery is essential for the websites because website visitors are scared of long texts. Instead, they rather websites which have images, graphics, and even several sliding images at once (IONOS Digitalguide). There are varieties of methods to create photo galleries. First method is embedding a Lightbox image gallery with source code in JavaScript. Through this method, which was developed by Lokesh Dhaka’s in 2005 (IONOS Digitalguide), the photo gallery will be implemented via HTML. Another method is the Image gallery extension for content management systems. This is the pre-built framework for users, even the users who have no IT knowledge. For example, for the open source content management system named WordPress, NextGen, Envira, and Polaroid are the beautiful, and accessible galleries.
+                  </p>
+              </div>
+
+              <div class="box">
+                  <div>
+                      <img class="gallery" src="images/hexagon.jpeg" alt="image">
+                      <p>1. Hexagon Gallery</p>
+                  </div>
+                  <div>
+                      <img class="gallery" src="images/zoomwalljs.jpeg" alt="image">
+                      <p>2. Zoomwall.js</p>
+                  </div>
+                  <div>
+                      <img class="gallery" src="images/simple_tab.jpeg" alt="image">
+                      <p>3. Simple Tab Gallery</p>
+                  </div>
+                  <div>
+                      <img class="gallery" src="images/purejs_thumbnail.jpeg" alt="image">
+                      <p>4. Pure JavaScript Fullscreen Gallery with Thumbnail Navigation</p>
+                  </div>
+                  <div>
+                      <img class="gallery" src="images/minimalist_touch.jpeg" alt="image">
+                      <p>5. Minimalist Touch-friendly Gallery in JavaScript</p>
+                  </div>
+                  <div>
+                      <img class="gallery" src="images/flow.jpeg" alt="image">
+                      <p>6. Flow Gallery</p>
+                  </div>
+                  <div>
+                      <img class="gallery" src="images/material_resp.jpeg" alt="image">
+                      <p>7. Material Responsive Photo Gallery</p>
+                  </div>
+              </div> <!-- ends class of box-->
+          </div> <!-- ends left column -->
+
+          <div> <!-- starts right column -->
               <h3>Works Cited:</h3>
               <p>“10 Best Carousel Plugins In JQuery/JavaScript/CSS (2021 Update).” JQuery Plugins, www.jqueryscript.net/blog/best-carousel.html.
               </p>
@@ -30,8 +53,9 @@
               </p>
               <p>“Web Design Basics: Creating a Photo Gallery for Your Website.” IONOS Digitalguide, www.ionos.com/digitalguide/websites/web-design/how-to-create-a-photo-gallery-for-your-website/.
               </p>
-          </aside>
-      </div>
+          </div> <!-- ends right column -->
+
+      </div> <!-- ends wrapper -->
   </body>
 
   </html>
