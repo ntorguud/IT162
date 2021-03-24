b1113fc623c6f85859dcd6777c7165aa42d9bad8
ntorguud03 <jenny.oyuka@gmail.com>
2021-03-24 01:26:13 +0000

    Fix div position

--- big/index.php
+++ big/index.php
@@ -2,18 +2,19 @@
 
 <body>
     <div id="wrapper">
-        <section>
+        <div>
             <h2><?= $PageID ?></h2>
             <p>Develop and place an updated version of the "big-multiple.php" contact form on this main page. Adjust its fields ta ask the basic questions you'd always need to ask any client to help determine if you can help them with their website needs.</p>
             <form><?php include '../includes/big-multiple.php' ?></form>
-        </section>
+        </div>
 
-        <aside>
+        <div>
             <h3>Basic Website Design Cycle</h3>
-        </aside>
-        <p>Provide at least 3 (linked) resources that help you and/or your web team with steps found in very basic website design and development cycle. For example, client surveys, wireframing tools, etc.</p>
-        <p>Here's a website to help get you started, <a href="https://business.tutsplus.com/articles/how-to-extract-the-facts-with-a-web-design-client-questionnaire--fsw-1830" target="top">https://business.tutsplus.com/articles/...</a>
-        </p>
+
+            <p>Provide at least 3 (linked) resources that help you and/or your web team with steps found in very basic website design and development cycle. For example, client surveys, wireframing tools, etc.</p>
+            <p>Here's a website to help get you started, <a href="https://business.tutsplus.com/articles/how-to-extract-the-facts-with-a-web-design-client-questionnaire--fsw-1830" target="top">https://business.tutsplus.com/articles/...</a>
+            </p>
+        </div>
     </div>
 
 </body>
