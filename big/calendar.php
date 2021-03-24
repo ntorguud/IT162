f6750531313309023733b4c3a6d8abdf901fe1b7
ntorguud03 <jenny.oyuka@gmail.com>
2021-03-24 05:18:17 +0000

    Add 2 kinds of Google Calendar view

--- big/calendar.php
+++ big/calendar.php
@@ -4,15 +4,23 @@
     <div id="wrapper">
 
         <div class="section">
-            <h2><?= $PageID ?></h2>
-            <p>Iframe a responsive Google Calendar and it should have one event on the claendar that is clickable with a description.</p>
+            <h2>Calendar (Google)</h2>
+            <div class="month-view embed-container">
+                <iframe src="https://calendar.google.com/calendar/embed?src=narant0310%40gmail.com&ctz=America%2FLos_Angeles" style="border: 0" width="90%" height="400" frameborder="0" scrolling="no">
+                </iframe>
+            </div>
+            <br>
+            <div class="agenda-view embed-container">
+                <iframe src="https://calendar.google.com/calendar/embed?src=en.usa%23holiday%40group.v.calendar.google.com&ctz=America%2FLos_Angeles" style="border: 0" width="90%" height="400" frameborder="0" scrolling="no"></iframe>
+            </div>
         </div>
 
         <div class="aside">
             <h3>Event info</h3>
-            <p>Add event text here.</p>
+            <p>~ Mar 6th, Saturday: Zoom Meeting on IT162</p>
+            <p>~ Mar 14th, Sunday: Day light saving time</p>
+            <p>~ Mar 17th, Wednesday: St. Patrick's Day</p>
         </div>
-
     </div>
 </body>
 
