17d98b3c5e5a3384bfd43ffc9b94f6024f2adc2b
Naran Torguud <ntorguud@gmail.com>
2021-03-29 00:24:00 +0000

    Create page

---
+++ template.php
@@ -0,0 +1,33 @@
+<?php include 'includes/header.php' ?>
+
+<div id="wrapper">
+    <h2>Welcome</h2>
+    <div class="aside">
+        <div class="blueberry">
+            <ul class="slides">
+                <li><img src="images/naran.png" alt="image" width="400"></li>
+                <li><img src="images/naran1.png" alt="images" width="400"></li>
+                <li><img src="images/naran2.png" alt="images" width="400"></li>
+            </ul>
+        </div>
+    </div>
+
+    <p>I am Naran Torguud. I was born and grew up in Mongolia. Since I was in a middle school, I have wondered how people discovered those scientific inventions, such as coding. However, I completed a bachelor's degree in Textile Design in my country, I have never forgotten my dream which is learning code. After I graduated, I started to work at Mongolia's leading Cashmere Fashion company called "Gobi Cashmere" for 5 years.
+    </p>
+</div>
+
+<!-- <footer>
+        <ul>
+            <li>&copy; 
+            <li>by <a href="http://hackergirl.xyz/index.php">Naran Torguud</a></li>
+            <li>All Rights Reserved</li>
+            <li><a href="https://validator.w3.org/check?uri=referer"><img src="images/html5.png" alt="html5"></a></li>
+            <li><a href="http://jigsaw.w3.org/css-validator/#validate_by_input">Valid CSS</a></li>
+        </ul>
+
+    </footer> -->
+
+<?php include 'includes/footer.php' ?>
+</body>
+
+</html>
